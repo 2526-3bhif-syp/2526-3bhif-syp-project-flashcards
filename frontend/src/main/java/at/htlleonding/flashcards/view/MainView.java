83eb7e7d9254a49579ef7d9f00a8a92d31f346ca
lorenz-parzer <l.parzer@students.htl-leonding.ac.at>
2026-04-02 23:14:38 +0200

    style: i changed the navbar to a borderpane, and its on the top of it. Under it there is supposed to be the main content

--- frontend/src/main/java/at/htlleonding/flashcards/view/MainView.java
+++ frontend/src/main/java/at/htlleonding/flashcards/view/MainView.java
@@ -1,15 +1,23 @@
 package at.htlleonding.flashcards.view;
 
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 public class MainView extends BorderPane {
     public MainView() {
         this.setStyle("-fx-background-color: white;");
         
-        NavbarView navbar = new NavbarView();
-        this.setTop(navbar);
-
         SidebarView sidebar = new SidebarView();
         this.setLeft(sidebar);
+
+        BorderPane contentArea = new BorderPane();
+        NavbarView navbar = new NavbarView();
+        contentArea.setTop(navbar);
+        
+        // Platzhalter für den Hauptinhalt
+        StackPane mainContent = new StackPane();
+        contentArea.setCenter(mainContent);
+        
+        this.setCenter(contentArea);
     }
 }
