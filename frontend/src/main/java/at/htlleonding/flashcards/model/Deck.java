d3205561b4380fad87fe657ec605009813509365
kxrim <k.yagci@students.htl-leonding.ac.at>
2026-04-05 11:45:36 +0200

    feat(model): add card ID and modification methods for edit/delete

--- frontend/src/main/java/at/htlleonding/flashcards/model/Deck.java
+++ frontend/src/main/java/at/htlleonding/flashcards/model/Deck.java
@@ -25,6 +25,19 @@ public class Deck {
         this.cards.add(card);
     }
     
+    public void removeCard(Card card) {
+        this.cards.removeIf(c -> c.getId().equals(card.getId()));
+    }
+    
+    public void updateCard(Card updatedCard) {
+        for (int i = 0; i < this.cards.size(); i++) {
+            if (this.cards.get(i).getId().equals(updatedCard.getId())) {
+                this.cards.set(i, updatedCard);
+                break;
+            }
+        }
+    }
+
     public void setCards(List<Card> cards) {
         this.cards = new ArrayList<>(cards);
     }
